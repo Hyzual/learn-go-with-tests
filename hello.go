8cac57eab447a7b71a4661721dab1c77cd008196
Joris Masson <joris.masson@enalean.com>
2020-04-18 15:16:36 +0200

    Spanish

--- hello.go
+++ hello.go
@@ -4,18 +4,26 @@ import (
 	"fmt"
 )
 
+const spanishParameter = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const defaultName = "World"
 
 // Hello : It says hello
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
-		return "Hello, World"
+		name = defaultName
 	}
+
+	if language == spanishParameter {
+		return spanishHelloPrefix + name
+	}
+
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 	// fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 	// fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
