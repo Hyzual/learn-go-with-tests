01acf791ab866a1fdb4d11e9aadb1ec82e74b37b
Joris Masson <hyzual@gmail.com>
2020-05-01 16:20:27 +0200

    Add mutex to the store

--- store.go
+++ store.go
@@ -1,11 +1,14 @@
 package main
 
+import "sync"
+
 func NewInMemoryPlayerStore() *InMemoryPlayerStore {
 	return &InMemoryPlayerStore{map[string]int{}}
 }
 
 type InMemoryPlayerStore struct {
 	store map[string]int
+	mu    sync.Mutex
 }
 
 func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
@@ -13,5 +16,7 @@ func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
 }
 
 func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.mu.Lock()
+	defer i.mu.Unlock()
 	i.store[name]++
 }
