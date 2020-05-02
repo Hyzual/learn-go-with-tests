4350f51ff93e741272f19cd41081b6b85c831410
Joris Masson <hyzual@gmail.com>
2020-05-02 13:17:53 +0200

    Delete InMemoryPlayerStore

--- store.go
+++ store.go
@@ -3,35 +3,7 @@ package main
 import (
 	"encoding/json"
 	"io"
-	"sync"
 )
-
-func NewInMemoryPlayerStore() *InMemoryPlayerStore {
-	return &InMemoryPlayerStore{store: map[string]int{}}
-}
-
-type InMemoryPlayerStore struct {
-	store map[string]int
-	mu    sync.Mutex
-}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return i.store[name]
-}
-
-func (i *InMemoryPlayerStore) RecordWin(name string) {
-	i.mu.Lock()
-	defer i.mu.Unlock()
-	i.store[name]++
-}
-
-func (i *InMemoryPlayerStore) GetLeague() []Player {
-	var league []Player
-	for name, wins := range i.store {
-		league = append(league, Player{name, wins})
-	}
-	return league
-}
 
 type FileSystemPlayerStore struct {
 	database io.ReadWriteSeeker
